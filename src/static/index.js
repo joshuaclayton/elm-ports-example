8491afc38f5870d8ca2bbfa518f81f1853b727c2
Joshua Clayton <joshua.clayton@gmail.com>
2017-10-16 08:19:57 -0400

    Add the remaining offices

--- src/static/index.js
+++ src/static/index.js
@@ -35,6 +35,86 @@ const flags = {
           longitude: -73.987007
         }
       }
+    },
+    {
+      id: 3,
+      name: "Austin",
+      address: {
+        street1: "600 Congress Ave.",
+        street2: "14th Floor",
+        city: "Austin",
+        state: "TX",
+        postalCode: "78701",
+        countryCode: "US",
+        latLon: {
+          latitude: 30.268611,
+          longitude: -97.743181
+        }
+      }
+    },
+    {
+      id: 4,
+      name: "London",
+      address: {
+        street1: "15 Long Lane, Barbican",
+        street2: "3rd Floor, Griffin Court",
+        city: "London",
+        state: "",
+        postalCode: "EC1A 9PN",
+        countryCode: "UK",
+        latLon: {
+          latitude: 51.519776,
+          longitude: -0.098997
+        }
+      }
+    },
+    {
+      id: 5,
+      name: "Raleigh",
+      address: {
+        street1: "555 Fayetteville St",
+        street2: "Suite 300",
+        city: "Raleigh",
+        state: "NC",
+        postalCode: "27601",
+        countryCode: "US",
+        latLon: {
+          latitude: 35.773245,
+          longitude: -78.638951
+        }
+      }
+    },
+    {
+      id: 6,
+      name: "San Francisco",
+      address: {
+        street1: "85 2nd St",
+        street2: "Suite 700",
+        city: "San Francisco",
+        state: "CA",
+        postalCode: "94105",
+        countryCode: "US",
+        latLon: {
+          latitude: 37.78844,
+          longitude: -122.399855
+        }
+      }
+    },
+    {
+      id: 7,
+      name: "Washington, D.C.",
+      address: {
+        street1: "810 7th St NE",
+        street2: "2nd Floor",
+        city: "Washington",
+        state: "DC",
+        postalCode: "20002",
+        countryCode: "US",
+        latLon: {
+          latitude: 38.900641,
+          longitude: -76.996502
+        }
+      }
     }
   ]
 };
